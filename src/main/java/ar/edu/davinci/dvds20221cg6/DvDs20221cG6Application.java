766aa70cbe6302365218a402090c60102a1a9089
h-kaname <luis--ao@hotmail.com>
2022-06-17 02:28:21 -0300

    cargamos dependencias de Spring

---
+++ src/main/java/ar/edu/davinci/dvds20221cg6/DvDs20221cG6Application.java
@@ -0,0 +1,13 @@
+package ar.edu.davinci.dvds20221cg6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DvDs20221cG6Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DvDs20221cG6Application.class, args);
+	}
+
+}
---
+++ src/test/java/ar/edu/davinci/dvds20221cg6/DvDs20221cG6ApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.edu.davinci.dvds20221cg6;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DvDs20221cG6ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
