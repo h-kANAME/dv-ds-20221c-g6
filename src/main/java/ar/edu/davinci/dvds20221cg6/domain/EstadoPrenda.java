41a23536b2637d54f1f65d400495889f44f9c9f0
Francisco José Montero <frajomon@gmail.com>
2022-12-14 20:29:43 -0300

    Cambios en Estado Prenda

--- src/main/java/ar/edu/davinci/dvds20221cg6/domain/EstadoPrenda.java
+++ src/main/java/ar/edu/davinci/dvds20221cg6/domain/EstadoPrenda.java
@@ -4,21 +4,27 @@ import java.util.LinkedList;
 import java.util.List;
 
 public enum EstadoPrenda {
-	
-	NUEVA("Nueva"),
-	LIQUIDACION("Liquidacion"),
-	PROMOCION("Promocion");
-	
+
+	NUEVA("Nueva", new PrendaNueva()),
+	LIQUIDACION("Liquidacion", new PrendaLiquidacion()),
+	PROMOCION("Promocion", new PrendaPromocion());
+
 	private String descripcion;
-	
-	private EstadoPrenda(String descripcion) {
+	private EstadoPrendaStrategy strategy;
+
+	private EstadoPrenda(String descripcion, EstadoPrendaStrategy strategy) {
 		this.descripcion = descripcion;
+		this.strategy = strategy;
 	}
-	
+
 	public String getDescripcion() {
 		return descripcion;
 	}
-	
+
+	public EstadoPrendaStrategy getStrategy() {
+		return strategy;
+	}
+
 	public static List<EstadoPrenda> getEstadoPrendas(){
 		List<EstadoPrenda> estadoPrendas = new LinkedList<EstadoPrenda>();
 		estadoPrendas.add(EstadoPrenda.NUEVA);
